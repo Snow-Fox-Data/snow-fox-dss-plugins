7913cd87f852c5addc22a739f3bcfe0cbc0c86e6
Ryan Moore <rymoore77@gmail.com>
2023-02-23 12:45:25 -0600

    no message

--- custom-recipes/sfd-monitor/recipe.py
+++ custom-recipes/sfd-monitor/recipe.py
@@ -273,7 +273,8 @@ def insert_records(vals, vals_str, errors, dss_jobs_df, dss_commit_df, dss_scena
                 qry += f"('{ACCT_UN}','{envt}',"
 
                 for c in dss_scenarios_df.columns:
-                    qry += f"'{row[c].replace('NaT', '')}',"
+                    val = row[c]
+                    qry += f"'{str(val)}',"
 
                 qry = qry[0:-1]
                 qry += '),'
